21d03ae512c7ee443d40d338d7f1be7bf14a0349
XuChongBo <xcbfreedom@xcbfreedommatoMacBook-Pro.local>
2013-11-04 15:31:21 +0800

    add row num limit

--- plot/printImagesData.R
+++ plot/printImagesData.R
@@ -1,5 +1,10 @@
 source("./myImagePlot.R")
-images=read.csv("../data/valid.csv") 
-b=as.matrix(images[23,2:785],rownames.force=NA) 
+images=read.csv("../data/valid.csv",nrows=10) 
+
+#get the 23th and cast to matrix
+b=as.matrix(images[6,2:785],rownames.force=NA) 
+
 b=matrix(b,28,28,byrow=TRUE) 
+
+#display the image 
 myImagePlot(b) 
