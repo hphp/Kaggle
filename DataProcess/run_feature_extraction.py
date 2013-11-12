0c59287313d8610be236b79fa7416e71c1d7f0fa
hphp <hphp@hphp-ThinkPad-T420.(none)>
2013-11-12 09:30:38 +0800

    chmod

--- DataProcess/run_feature_extraction.py
+++ DataProcess/run_feature_extraction.py
@@ -2,12 +2,12 @@
 
 import os
 
-piclist = os.listdir("train/")
+piclist = os.listdir("/home/hphp/Documents/data/Kaggle/DogVsCatData/test1/")
 t_range = len(piclist)
 period = 1000
 total = int(t_range/period)
 print total
-for rr in range(total):
+for rr in range(200,total):
     start = rr * 1000
     end = min((rr+1)*1000,t_range)
     cmd = "python feature_extraction.py " + str(start) +  " " + str(end)
