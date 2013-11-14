c542d91303c6aa4dfc031abe59f4ff1167351ecf
hphp <hphpcarrot@gmail.com>
2013-11-14 21:12:50 +0800

    DogVsCat texture_tiles

---
+++ DogVsCatMethod/DogVsCat_get_texture_tiles.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+'''
+    2013-11-14
+    written by hp_carrot
+
+    Part2:
+    1.get texture tiles info with shape : [index,patch_size,patch_size,3]
+    2. record in pickle
+
+'''
