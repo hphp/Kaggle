f0237b0def7dbe2e874ab8dea8399f98a9b46bbf
hphp <hphpcarrot@gmail.com>
2013-11-18 16:14:54 +0800

    1.modify lenet to train_model.pkl + test from pkl

---
+++ DogVsCatMethod/DogVsCat_convolution_mlp_test.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python
+'''
+written by hp_carrot
+2013-11-18
+test with trained model
+lenet
+'''
+
+import cPickle
+import gzip
+import os
+import sys
+import time
+
+import numpy
+
+import theano
+theano.config.floatX='float32'
+import theano.tensor as T
+from theano.tensor.signal import downsample
+from theano.tensor.nnet import conv
+
+if not "../DL_Method/" in sys.path:
+    sys.path.append("../DL_Method/")
+from logistic_sgd import LogisticRegression
+from mlp import HiddenLayer
+from convolutional_mlp import LeNetConvPoolLayer
+import load_data
+
+DataHome = "../../data/Kaggle/MNISTData/"
+
+def evaluate_lenet5(dataset=DataHome, \
+                    nkerns=[20, 50], batch_size=5):
+    """ Demonstrates lenet on MNIST dataset
+
+    :type learning_rate: float
+    :param learning_rate: learning rate used (factor for the stochastic
+                          gradient)
+
+    :type n_epochs: int
+    :param n_epochs: maximal number of epochs to run the optimizer
+
+    :type dataset: string
+    :param dataset: path to the dataset used for training /testing (MNIST here)
+
+    :type nkerns: list of ints
+    :param nkerns: number of kernels on each layer
+    """
+
+    rng = numpy.random.RandomState(23455)
+
+    trained_model_pkl = open("tmp.file", 'r')
+    trained_model_state_list = cPickle.load(trained_model_pkl)
+    layer0_state, layer1_state, layer2_state, layer3_state = trained_model_state_list 
+
+    datasets = load_data.load_test_data(dataset)
+
+    test_set_x, test_set_y = datasets[0]
+
+    # compute number of minibatches for training, validation and testing
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0]
+    n_test_batches /= batch_size
+
+    # allocate symbolic variables for the data
+    index = T.lscalar()  # index to a [mini]batch
+    x = T.matrix('x')   # the data is presented as rasterized images
+    y = T.ivector('y')  # the labels are presented as 1D vector of
+                        # [int] labels
+
+    ishape = (28, 28)  # this is the size of MNIST images
+
+    ######################
+    # BUILD ACTUAL MODEL #
+    ######################
+    print '... building the model'
+
+    # Reshape matrix of rasterized images of shape (batch_size,28*28)
+    # to a 4D tensor, compatible with our LeNetConvPoolLayer
+    layer0_input = x.reshape((batch_size, 1, 28, 28))
+
+    # Construct the first convolutional pooling layer:
+    # filtering reduces the image size to (28-5+1,28-5+1)=(24,24)
+    # maxpooling reduces this further to (24/2,24/2) = (12,12)
+    # 4D output tensor is thus of shape (batch_size,nkerns[0],12,12)
+    layer0 = LeNetConvPoolLayer(rng, input=layer0_input, \
+            image_shape=(batch_size, 1, 28, 28), \
+            filter_shape=(nkerns[0], 1, 5, 5), poolsize=(2, 2), \
+            W=layer0_state[0], b=layer0_state[1] \
+            )
+
+    # Construct the second convolutional pooling layer
+    # filtering reduces the image size to (12-5+1,12-5+1)=(8,8)
+    # maxpooling reduces this further to (8/2,8/2) = (4,4)
+    # 4D output tensor is thus of shape (nkerns[0],nkerns[1],4,4)
+    layer1 = LeNetConvPoolLayer(rng, input=layer0.output,
+            image_shape=(batch_size, nkerns[0], 12, 12),
+            filter_shape=(nkerns[1], nkerns[0], 5, 5), poolsize=(2, 2), \
+            W=layer1_state[0], b=layer1_state[1] \
+            )
+
+    # the TanhLayer being fully-connected, it operates on 2D matrices of
+    # shape (batch_size,num_pixels) (i.e matrix of rasterized images).
+    # This will generate a matrix of shape (20,32*4*4) = (20,512)
+    layer2_input = layer1.output.flatten(2)
+
+    # construct a fully-connected sigmoidal layer
+    layer2 = HiddenLayer(rng, input=layer2_input, n_in=nkerns[1] * 4 * 4,
+                         n_out=500, activation=T.tanh,\
+                         W=layer2_state[0], b=layer2_state[1] \
+                         )
+
+    # classify the values of the fully-connected sigmoidal layer
+    layer3 = LogisticRegression(input=layer2.output, n_in=500, n_out=10, \
+                                    W=layer3_state[0], b=layer3_state[1] \
+                                )
+
+    print "predicting"
+    start_time = time.clock()
+    # create a function to compute the mistakes that are made by the model
+    test_results = theano.function(inputs=[index],
+            outputs= layer3.y_pred,
+            givens={
+                x: test_set_x[index * batch_size: (index + 1) * batch_size]})
+
+    
+    test_res = [test_results(i)
+        for i in xrange(n_test_batches)]
+    print test_res
+    
+    end_time = time.clock()
+    print >> sys.stderr, ('The code for file ' +
+                          os.path.split(__file__)[1] +
+                          ' ran for %.2fm' % ((end_time - start_time) / 60.))
+
+if __name__ == '__main__':
+    start_sec = time.time()
+    evaluate_lenet5()
+    end_sec = time.time()
+    print 'practical using time : %.2fm ' % ((end_sec - start_sec) / 60.)
+
+def experiment(state, channel):
+    evaluate_lenet5(state.learning_rate, dataset=state.dataset)
