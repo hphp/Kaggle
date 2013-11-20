45d8d4a23db28df0728b3e822a14df7568231016
hphp <hphpcarrot@gmail.com>
2013-11-20 11:53:17 +0800

    DogVsCat:train 3 classes model

--- DataProcess/DogVsCat_head_feature.py
+++ DataProcess/DogVsCat_head_feature.py
@@ -14,11 +14,12 @@ import colorsys
 import time
 import os
 import random
+import cv2
 import transform_data_to_format as tdtf
 
 DataHome = "../../data/Kaggle/DogVsCatData/"
-train_feature_filename = DataHome + "DogVsCat_head_train_feature_2500.csv"
-valid_feature_filename = DataHome + "DogVsCat_head_valid_feature_2500.csv"
+train_feature_filename = DataHome + "DogVsCat_head_train_feature_3c_2500.csv"
+valid_feature_filename = DataHome + "DogVsCat_head_valid_feature_3c_2500.csv"
 print "going through the whole feature space to check if corresponding place is 1."
 print "resizing"
 
@@ -30,30 +31,39 @@ def img_label(img_name):
         label = 0
 
     return label
-
 img_name_list = os.listdir(DataHome + "head_images/")
 features_list = []
 for img_name in img_name_list:
-    img = Image.open(open(DataHome + "head_images/" + img_name))
-    img = img.resize((50,50),Image.ANTIALIAS)
-    img = numpy.asarray(img, dtype='int32')
+    #img = Image.open(DataHome + "head_images/" + img_name)
+    img = cv2.imread(DataHome + "head_images/" + img_name)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # to resize 
+    w,h=(50,50)
+    img = cv2.resize(img,(w,h),interpolation=cv2.INTER_LINEAR)
+    #print "after resize and gray:",type(img),img.shape,img.dtype
+
+    #show the gray img
+    #cv2.imshow("w2",img)
+    #cv2.waitKey(0)
+
+    #reshape (h,w) to (h*w,) 
+    img=img.reshape(w*h) 
     feature= []
     feature.append(img_label(img_name))
-    shape_len = len(img.shape)
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            cur_v = 0
-            if shape_len < 3:
-                cur_v = img[i][j]
-            else:
-                for ch_v in img[i][j]:
-                   cur_v += ch_v 
-                cur_v /= 3
-            feature.append(cur_v)
-
+    for f_v in img:
+        feature.append(f_v)
     features_list.append(feature)
 
-print len(features_list)
+features_n = len(features_list)
+
+for i in range(features_n):
+    feature = [2]
+    for i in range(2500):
+        feature.append(random.randrange(0,256))
+    features_list.append(feature)
+
+print len(features_list),len(features_list[0]),len(features_list[-1])
 train_index_list = random.sample(range(len(features_list)), len(features_list)/2 )
 train_features_list = []
 for i in train_index_list:
@@ -64,7 +74,7 @@ for i in range(len(features_list)):
         continue
     valid_features_list.append(features_list[i])
 
-#print len(train_features_list)
-#print len(valid_features_list)
+print len(train_features_list)
+print len(valid_features_list)
 tdtf.write_content_to_csv(train_features_list,train_feature_filename)
 tdtf.write_content_to_csv(valid_features_list,valid_feature_filename)
