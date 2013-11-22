8e1c268337547de5c946536a479f5e80d8e9b40d
hphp <hphpcarrot@gmail.com>
2013-11-22 22:23:02 +0800

    sign up shared_dataset

--- DogVsCatMethod/load_data.py
+++ DogVsCatMethod/load_data.py
@@ -7,8 +7,32 @@ import sys;
 if not "../DataProcess/" in sys.path:
     sys.path.append("../DataProcess/")
 import transform_data_to_format as tdtf
+def shared_dataset(data_xy, borrow=True):
+    """ Function that loads the dataset into shared variables
 
-def load_test_data(dataset):
+    The reason we store our dataset in shared variables is to allow
+    Theano to copy it into the GPU memory (when code is run on GPU).
+    Since copying data into the GPU is slow, copying a minibatch everytime
+    is needed (the default behaviour if the data is not in a shared
+    variable) would lead to a large decrease in performance.
+    """
+    data_x, data_y = data_xy
+    shared_x = theano.shared(numpy.asarray(data_x,
+                                           dtype=theano.config.floatX),
+                             borrow=borrow)
+    shared_y = theano.shared(numpy.asarray(data_y,
+                                           dtype=theano.config.floatX),
+                             borrow=borrow)
+    # When storing data on the GPU it has to be stored as floats
+    # therefore we will store the labels as ``floatX`` as well
+    # (``shared_y`` does exactly that). But during our computations
+    # we need them as ints (we use labels as index, and if they are
+    # floats it doesn't make sense) therefore instead of returning
+    # ``shared_y`` we will have to cast it to int. This little hack
+    # lets ous get around this issue
+    return shared_x, T.cast(shared_y, 'int32')
+
+def load_test_data(dataset, read_limit=None):
     ''' Loads test dataset
 
     :type dataset: string
@@ -21,7 +45,7 @@ def load_test_data(dataset):
     #numpy.ndarray of 1 dimensions (vector)) that have the same length as
     #the number of rows in the input. It should give the target
     #target to the example with the same index in the input.
-    test_set = tdtf.read_test_data_xy_to_ndarray(dataset + "test.csv",limit=10)
+    test_set = tdtf.read_test_data_xy_to_ndarray(dataset + "test.csv", limit=read_limit)
 
     def shared_dataset(data_xy, borrow=True):
         """ Function that loads the dataset into shared variables
