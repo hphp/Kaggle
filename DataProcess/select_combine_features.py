2f0eb9526d92634236c2daddc0f3534067b38187
hphp <hphpcarrot@gmail.com>
2013-12-07 13:20:59 +0800

    DataProcess/select_combine_features.py add a rate when selecting features

--- DataProcess/select_combine_features.py
+++ DataProcess/select_combine_features.py
@@ -15,6 +15,7 @@ to_valid_feature_filename = DataHome + "DogVsCat_head_valid_feature_3c_2500.csv"
 from_train_feature_filename = DataHome + "DogVsCat_head_train_feature_2500.csv"
 from_valid_feature_filename = DataHome + "DogVsCat_head_valid_feature_2500.csv"
 bg_feature_filename = DataHome + "DogVsCat_bg_feature_2500.csv"
+to_feature_rate = [0.5, 0.5]
 
 from_feature_fname_list = [bg_feature_filename, from_train_feature_filename, from_valid_feature_filename]
 to_feature_fname_list = [to_train_feature_filename, to_valid_feature_filename]
@@ -23,6 +24,12 @@ DataHome = "../../data/Kaggle/CIFAR-10/"
 to_train_feature_filename = DataHome + "CIFAR_train_feature.csv"
 to_valid_feature_filename = DataHome + "CIFAR_valid_feature.csv"
 from_feature_filename = DataHome + "train_feature_Cls_pixelv.csv"
+to_feature_rate = [0.5, 0.5]
+
+to_train_feature_filename = DataHome + "CIFAR_train4_feature.csv"
+to_valid_feature_filename = DataHome + "CIFAR_valid1_feature.csv"
+from_feature_filename = DataHome + "train_feature_Cls_pixelv.csv"
+to_feature_rate = [0.8, 0.2]
 
 from_feature_fname_list = [from_feature_filename]
 to_feature_fname_list = [to_train_feature_filename, to_valid_feature_filename]
@@ -32,7 +39,7 @@ for fname in from_feature_fname_list:
     t_f_list = tdtf.read_feature_from_csv(filname=fname, limit=None, header_n=0)
     features_list += (t_f_list)
 
-train_index_list = random.sample(range(len(features_list)), len(features_list)/2 )
+train_index_list = random.sample(range(len(features_list)), int(len(features_list)*to_feature_rate[0]) )
 train_features_list = []
 for i in train_index_list:
     train_features_list.append(features_list[i])
